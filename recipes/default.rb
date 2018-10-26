2d69249135ecea6a008e96d3a78cc8cc6230af12
Tech-A07 <rlecoutre@spartaglobal.com>
2018-10-26 12:32:35 +0100

    initial

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,31 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+apt_update 'update_sources' do
+  action :update
+end
+
+package "nginx"
+
+service "nginx" do
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
+end
+
+include_recipe "nodejs"
+nodejs_npm 'pm2'
+
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  notifies :restart, 'service[nginx]'
+end
+
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+  notifies :restart, 'service[nginx]'
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies :restart, 'service[nginx]'
+end
